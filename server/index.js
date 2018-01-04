9f8422e66f948cd804f63ef85d07264eedf90103
Aries Cs <aries0d0f@gmail.com>
2018-01-04 20:43:34 +0800

    Correction config file path.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app         = new (require('koa'))()
 const path        = require('path')
-const config      = require('./config.json')
+const config      = require('../config.json')
 
 app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
 app.listen(config.port)
