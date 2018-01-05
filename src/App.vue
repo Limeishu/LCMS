219f81ea3b92f06677d314d17216c28b2dae16fe
Aries Cs <aries0d0f@gmail.com>
2018-01-05 10:03:24 +0800

    Binding components to mount.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Navbar></Navbar>
+    <div class="main">
+      <Sidebar></Sidebar>
+      <mainFooter></mainFooter>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+@import './assets/scss/main.scss'
+</style>
+
+
+<script>
+import { Navbar, mainFooter, Sidebar } from '@/components/layout'
+export default {
+  components: {
+    Navbar: Navbar,
+    mainFooter: mainFooter,
+    Sidebar: Sidebar
+  }
+}
+</script>
