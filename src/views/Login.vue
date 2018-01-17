fd85539609ab3e6c43b659fec8d9332fc5c23db9
Aries Cs <aries0d0f@gmail.com>
2018-01-17 15:52:50 +0800

    Add basic methods for login components.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,35 @@
 <template>
-  
+  <div id="login">
+    <input type="text" v-model="user.username">
+    <input type="password" v-model="user.password">
+  </div>
 </template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+  export default {
+    data () {
+      return {
+        msg: '',
+        user: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    computed: {
+      ...mapGetters(['isLogin'])
+    },
+    methods: {
+      ...mapActions(['userLogin']),
+      async login () {
+        this.msg = ''
+        try {
+          await this.userLogin(this.user)
+        } catch (err) {
+          this.msg = err.message
+        }
+      }
+    }
+  }
+</script>
