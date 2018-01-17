62eb2f0abb232f6eb409a9d73786772f14e78ac9
Aries Cs <aries0d0f@gmail.com>
2018-01-17 15:53:14 +0800

    Add login check to root components.

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,29 @@
 
 
 <script>
+  import { mapGetters } from 'vuex'
   import { Navbar, mainFooter, Sidebar } from '@/components/layout'
   export default {
     components: {
       Navbar: Navbar,
       mainFooter: mainFooter,
       Sidebar: Sidebar
+    },
+    computed: {
+      ...mapGetters(['isLogin'])
+    },
+    created () {
+      this.checkLogin()
+    },
+    methods: {
+      checkLogin () {
+        this.isLogin ? null : this.$router.push('/login')
+      }
+    },
+    watch: {
+      $route (route) {
+        this.checkLogin()
+      }
     }
   }
 </script>
