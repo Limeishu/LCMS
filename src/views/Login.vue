06fa5dfc99103922eb9773935dba9e09ae445b83
Aries Cs <aries0d0f@gmail.com>
2018-01-18 18:07:35 +0800

    Add Google Recaptcha and add it to login page.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="login">
+    <div class="msg"><p>{{ msg }}</p></div>
     <input type="text" v-model="user.username">
     <input type="password" v-model="user.password">
-    <div class="button" @click="login()">Login</div>
+    <vue-recaptcha class="recaptcha" @verify="onVerify" ref="recaptcha" sitekey="6LefXEEUAAAAAPN6fbJMgC6bEOB2n_4b_CxRXIqv"></vue-recaptcha>
+    <div class="button" @click="login()"><span>Login</span></div>
   </div>
 </template>
 
 <script>
+  import VueRecaptcha               from 'vue-recaptcha'
   import { mapActions, mapGetters } from 'vuex'
   export default {
     data () {
       return {
         msg: '',
+        recaptcha: false,
         user: {
           username: '',
           password: ''
         }
       }
+    },
+    components: {
+      VueRecaptcha
     },
     computed: {
       ...mapGetters(['isLogin'])
@@ -25,11 +32,18 @@
       ...mapActions(['userLogin']),
       async login () {
         this.msg = ''
-        try {
-          await this.userLogin(this.user)
-        } catch (err) {
-          this.msg = err.message
+        if (!this.recaptcha) {
+          this.msg = `請先進行機器人驗證`
+        } else {
+          try {
+            await this.userLogin(this.user)
+          } catch (err) {
+            this.msg = `帳號或密碼錯誤，請再試一次。`
+          }
         }
+      },
+      onVerify () {
+        this.recaptcha = true
       }
     }
   }
