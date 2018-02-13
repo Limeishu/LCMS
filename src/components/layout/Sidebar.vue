a9669d16c544a77be5a3f78e489e9b1d83b0be32
Aries Cs <aries0d0f@gmail.com>
2018-02-13 08:15:56 +0800

    Lint code.

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="sidebar">
     <ul>
-      <li v-for="(item, i) in menu">
+      <li v-for="(item, i) in menu" :key="i">
         <router-link :to="item.path" v-if="item.path">
           <font-awesome-icon v-if="item.meta.icon" :icon="item.meta.icon" />
           <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
         </router-link>
         <ul v-if="item.children">
-          <li v-for="item in item.children">
+          <li v-for="(item, j) in item.children" :key="j">
             <router-link :to="item.path" v-if="item.path">
               <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
             </router-link>
