54ff1b3b36f7623db658ffe178b8648908a9797f
Aries Cs <aries0d0f@gmail.com>
2018-02-13 08:05:59 +0800

    Display nickname if available and adjust layout.

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar">
     <h1 class="logo">李梅樹網站管理系統<div class="version">v{{ version }}</div></h1>
-    <div class="user" v-if="isLogin"><p>{{ user.username }}</p><p @click="logout(), $router.push('/login')">登出</p></div>
+    <div class="user" v-if="isLogin"><p><font-awesome-icon icon="user" /><span>{{ user.meta.nickname ? user.meta.nickname : user.username }}</span></p><p @click="logout(), $router.push('/login')">登出</p></div>
   </div>
 </template>
 
