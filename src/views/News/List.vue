ebe459e9fb4532cb3a23df22840492959a52b833
Aries Cs <aries0d0f@gmail.com>
2018-02-14 13:30:33 +0800

    Add news list.

--- src/views/News/List.vue
+++ src/views/News/List.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>
-
+  <div class="list-container">
+    <div class="list head">
+      <div class="title"><p>標題</p></div>
+      <div class="content"><p>內容預覽</p></div>
+      <div class="action"><p>編輯</p><p>查看</p><p>刪除</p></div>
+    </div>
+    <div class="list" v-for="(item, i) in news.data" :key="i">
+      <div class="title">
+        <p>{{ item.title }}</p>
+      </div>
+      <div class="content">
+        <p>{{ `${item.content.substring(0, 50)}...` }}</p>
+      </div>
+      <div class="action">
+        <p @click="$router.push(`news/edit/${item._id}`)"><font-awesome-icon icon="edit" /></p>
+        <p @click="openView(item._id)"><font-awesome-icon icon="eye" /></p>
+        <p @click="deleteAction(item._id)"><font-awesome-icon icon="minus-circle" /></p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['news'])
+    },
+    mounted () {
+      this.getAllNews()
+    },
+    methods: {
+      ...mapActions(['getAllNews', 'deleteNews']),
+      async deleteAction (nid) {
+        if (confirm('確定要刪除這篇文章？此動作不可回復')) {
+          let success = await this.deleteNews(nid)
+          if (success) {
+            this.getAllNews()
+          } else {
+            alert('操作失敗！請洽系統管理員')
+          }
+        }
+      },
+      openView (nid) {
+        window.open(`https://limeishu.org.tw/culture/news/${nid}`)
+      }
+    }
+  }
+</script>
