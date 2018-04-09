c0c3eeb8d43d82beaa972b5bce94e064cd69d691
Aries Cs <aries0d0f@gmail.com>
2018-04-09 21:01:10 +0800

    Modify structure.

--- src/views/News/Add.vue
+++ src/views/News/Add.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="editor-container paragraph">
-    <div class="input-box">
-      <input type="text" v-model="news.title" autocomplete="off" required>
-      <span class="bar"></span>
-      <label><span>標題</span></label>
+  <div>
+    <div class="editor-container paragraph">
+      <div class="input-box">
+        <input type="text" v-model="news.title" autocomplete="off" required>
+        <span class="bar"></span>
+        <label>
+          <span>標題</span>
+        </label>
+      </div>
+      <span :class="{ 'active': news.meta.image }">{{ img.length === 0 ? '先上傳圖片至內文編輯區' : '請選擇主要圖片' }}</span>
+      <div class="img-list">
+        <div class="image" :class="{ 'active': news.meta.image === url }" :style="{ 'background-image': `url(${url})` }" alt="" v-for="(url , i) in img"
+          :key="i" @click="news.meta.image = url"></div>
+      </div>
+      <div class="input-box">
+        <input type="date" v-model="news.date" autocomplete="off" required>
+        <span class="bar"></span>
+        <label>
+          <span>活動結束日期</span>
+        </label>
+      </div>
+      <span>內文編輯區</span>
+      <vue-editor class="editor" useCustomImageHandler @imageAdded="handleImageAdded" v-model="news.paragraph"></vue-editor>
+      <div class="button" @click="news.permission = 0, send()">
+        <span>發佈</span>
+      </div>
+      <div class="button" @click="news.permission = -1, send()">
+        <span>儲存為草稿</span>
+      </div>
     </div>
-    <span :class="{ 'active': news.meta.image }">{{ img.length === 0 ? '先上傳圖片至內文編輯區' : '請選擇主要圖片' }}</span>
-    <div class="img-list">
-      <div class="image" :class="{ 'active': news.meta.image === url }" :style="{ 'background-image': `url(${url})` }" alt="" v-for="(url , i) in img" :key="i" @click="news.meta.image = url"></div>
-    </div>
-    <div class="input-box">
-      <input type="date" v-model="news.date" autocomplete="off" required>
-      <span class="bar"></span>
-      <label><span>活動結束日期</span></label>
-    </div>
-    <span>內文編輯區</span>
-    <vue-editor class="editor"
-      useCustomImageHandler
-      @imageAdded="handleImageAdded"
-      v-model="news.paragraph"></vue-editor>
-      <div class="button" @click="news.permission = 0, send()"><span>發佈</span></div>      
-      <div class="button" @click="news.permission = -1, send()"><span>儲存為草稿</span></div>
   </div>
 </template>
 
--- src/views/News/List.vue
+++ src/views/News/List.vue
@@ -1,22 +1,38 @@
 <template>
-  <div class="list-container">
-    <div class="list head">
-      <div class="title"><p>標題</p></div>
-      <div class="content"><p>內容預覽</p></div>
-      <div class="action"><p>編輯</p><p>查看</p><p>刪除</p></div>
-    </div>
-    <div class="list" v-for="(item, i) in news.data" :key="i">
-      <div class="title">
-        <span class="tag" v-if="item.permission === -1">草稿</span>
-        <p>{{ item.title }}</p>
+  <div>
+    <div class="list-container">
+      <div class="list head">
+        <div class="title">
+          <p>標題</p>
+        </div>
+        <div class="content">
+          <p>內容預覽</p>
+        </div>
+        <div class="action">
+          <p>編輯</p>
+          <p>查看</p>
+          <p>刪除</p>
+        </div>
       </div>
-      <div class="content">
-        <p>{{ `${item.content.substring(0, 50)}...` }}</p>
-      </div>
-      <div class="action">
-        <p @click="$router.push(`news/edit/${item._id}`)"><font-awesome-icon icon="edit" /></p>
-        <p @click="openView(item._id)"><font-awesome-icon icon="eye" /></p>
-        <p @click="deleteAction(item._id)"><font-awesome-icon icon="minus-circle" /></p>
+      <div class="list" v-for="(item, i) in news.data" :key="i">
+        <div class="title">
+          <span class="tag" v-if="item.permission === -1">草稿</span>
+          <p>{{ item.title }}</p>
+        </div>
+        <div class="content">
+          <p>{{ `${item.content.substring(0, 50)}...` }}</p>
+        </div>
+        <div class="action">
+          <p @click="$router.push(`news/edit/${item._id}`)">
+            <font-awesome-icon icon="edit" />
+          </p>
+          <p @click="openView(item._id)">
+            <font-awesome-icon icon="eye" />
+          </p>
+          <p @click="deleteAction(item._id)">
+            <font-awesome-icon icon="minus-circle" />
+          </p>
+        </div>
       </div>
     </div>
   </div>
