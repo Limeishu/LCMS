f80c2370924cf85bf0b6d42b7ff02b77da64f3cb
Aries Cs <aries0d0f@gmail.com>
2018-04-29 00:00:52 +0800

    Fix logicial bug.

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar">
     <h1 class="logo">李梅樹網站管理系統<div class="version">v{{ version }}</div></h1>
-    <div class="user" v-if="isLogin"><p @click="$router.push('/user')"><font-awesome-icon icon="user" /><span>{{ user.meta.nickname ? user.meta.nickname : user.username }}</span></p><p @click="logout(), $router.push('/login')">登出</p></div>
+    <div class="user" v-if="isLogin"><p @click="$router.push('/user')"><font-awesome-icon icon="user" /><span>{{ user.meta ? user.meta.nickname : user.username }}</span></p><p @click="logout(), $router.push('/login')">登出</p></div>
   </div>
 </template>
 
