a2a8103a452e80ddc4c4d04bedab933f48881e51
Aries Cs <aries0d0f@gmail.com>
2018-10-29 14:52:08 +0800

    Structure: renew icon library

--- src/main.js
+++ src/main.js
@@ -11,45 +11,18 @@ import App          from './App'
 import router       from './router'
 import store        from './store'
 
-import FontAwesomeIcon                from '@fortawesome/vue-fontawesome'
-import FontAwesome                    from '@fortawesome/fontawesome'
-import { }                                     from '@fortawesome/fontawesome-free-brands'
-import {
-         faAngleLeft,
-         faAngleRight,
-         faCircle,
-         faCircleNotch,
-         faHome,
-         faCaretDown,
-         faHeart,
-         faCartPlus,
-         faPlus,
-         faMinus,
-         faCreditCard,
-         faTruck,
-         faEnvelope,
-         faKey,
-         faTimes,
-         faCheckCircle,
-         faExclamationCircle,
-         faCheck,
-         faGift,
-         faShareAlt,
-         faColumns,
-         faFile,
-         faNewspaper,
-         faUser,
-         faListAlt,
-         faEdit,
-         faEye,
-         faMinusCircle,
-         faArrowUp,
-         faArrowDown,
-         faServer
-       }            from '@fortawesome/fontawesome-free-solid'
+// Fontawesome
+import { FontAwesomeIcon }  from '@fortawesome/vue-fontawesome'
+import { library }          from '@fortawesome/fontawesome-svg-core'
+import { fab }              from '@fortawesome/free-brands-svg-icons'
+import { fas }              from '@fortawesome/free-solid-svg-icons'
+import { far }              from '@fortawesome/free-regular-svg-icons'
 
-FontAwesome.library.add(faAngleLeft, faAngleRight, faUser, faCircle, faServer, faCircleNotch, faArrowUp, faArrowDown, faListAlt, faMinusCircle, faEye, faHome, faCaretDown, faEdit, faHeart, faCartPlus, faPlus, faMinus, faCreditCard, faTruck, faEnvelope, faKey, faTimes, faCheckCircle, faExclamationCircle, faCheck, faGift, faShareAlt, faColumns, faFile, faNewspaper)
-Vue.component('FontAwesomeIcon', FontAwesomeIcon)
+library.add(fab)
+library.add(fas)
+library.add(far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.component('VueEditor', VueEditor)
 
 Vue.router = router
@@ -72,4 +45,4 @@ const app = new Vue({
   ...App
 }).$mount('#LCMS')
 
-export { app, router, store, FontAwesome }
+export { app }
