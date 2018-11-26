5ac466d07b0dceae3e7ea1d04c5eb6eb13ec98f3
Aries Cs <aries0d0f@gmail.com>
2018-11-26 23:24:43 +0800

    Fix: webcam key accessibility timeout

--- src/views/Monit.vue
+++ src/views/Monit.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="monit-container">
       <div class="monit" v-for="i in 16" :key="`cam-${i}`">
-        <img :src="`${monitorCfg.server}/mjpeg?cam=${i}&IDKey=${monitorCfg.key}`" alt="">
+        <img :src="`${monitorCfg.server}/mjpeg?cam=${i}&IDKey=${monitServerKey}`" alt="">
         <p><font-awesome-icon icon="circle" /> CAM {{ i }}</p>
       </div>
     </div>
@@ -10,12 +10,23 @@
 </template>
 
 <script>
+  import axios from 'axios'
   import config from '../../server/config.json'
   export default {
     data () {
       return {
         nowFouceCamNum: 1,
-        monitorCfg: config.monitor
+        monitorCfg: config.monitor,
+        monitServerKey: ''
+      }
+    },
+    mounted () {
+      this.getKey()
+    },
+    methods: {
+      async getKey () {
+        let res = await axios.get(`${this.monitorCfg.server}/getKey`)
+        this.monitServerKey = res.data
       }
     }
   }
