e2f3bc64c5a3258f1bda3b78f2026218d8322312
Aries Cs <aries0d0f@gmail.com>
2018-12-30 13:10:09 +0800

    Layout: add sidebar RWD

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="sidebar">
+  <div class="sidebar" :class="{'active' : toggle}">
     <ul>
+      <li class="toggle-btn" @click="toggle = !toggle">
+        <a>
+          <font-awesome-icon v-if="!toggle" :icon="['fas', 'bars']" />
+          <font-awesome-icon v-if="toggle" :icon="['fas', 'arrow-left']" />
+        </a>
+      </li>
       <li v-for="(item, i) in menu" :key="i" v-if="item.meta.permission >= user.permission">
         <router-link :to="item.path" v-if="item.path" :class="{ 'active': $route.path === item.path }">
           <font-awesome-icon v-if="item.meta.icon" :icon="item.meta.icon" />
           <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
         </router-link>
-        <ul v-if="item.children">
-          <li v-for="(item, j) in item.children" :key="j">
-            <router-link :to="item.path" v-if="item.path">
-              <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
-            </router-link>
-          </li>
-        </ul>
       </li>
     </ul>
   </div>
@@ -21,8 +20,18 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data () {
+    return {
+      toggle: false
+    }
+  },
   computed: {
     ...mapGetters({ menu: 'menuitems', user: 'userInfo' })
+  },
+  watch: {
+    $route (route) {
+      this.toggle = false
+    }
   }
 }
 </script>
