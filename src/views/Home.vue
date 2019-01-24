145ce283813d3288d093b102f8b2a4a803482bd6
Aries Cs <aries0d0f@gmail.com>
2019-01-24 11:17:59 +0800

    Content: update google data studio link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <div class="analystic">
-      <iframe src="https://datastudio.google.com/embed/reporting/1Peiw4EhOQ4lxHiviT7wwQMs86-AY5k0Q/page/FVjQ" frameborder="0"></iframe>
+      <iframe src="https://datastudio.google.com/embed/reporting/1wRINPV7FyF-SOa7jtNL0TlubFIw-Eov_/page/FVjQ" frameborder="0"></iframe>
     </div>
   </div>
 </template>
